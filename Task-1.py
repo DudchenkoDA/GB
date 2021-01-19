31c750fc8edaf743cea9053d57bb2a961534ae44
DudchenkoDA <DDA6@mail.ru>
2021-01-19 13:45:19 +0300

    Added str output

--- Task-1.py
+++ Task-1.py
@@ -3,18 +3,19 @@
 # запросите у пользователя несколько чисел и строк и
 # сохраните в переменные, выведите на экран.
 print("Сalculations of integers a and b")
-a = int(input("Input int number a\n"))
-b = int(input("Input int number b\n"))
-s = a/b
-t = a % b
+a = input("Enter your name\n")
+b = int(input("Input int number a\n"))
+c = int(input("Input int number b\n"))
+s = b / c
+t = b % c
 
-print("answer with /", str(s))
-print("answer with % -", str(t))
+print(str(a),"answer with /", str(s))
+print(str(a),"answer with % -", str(t))
 
 
-if a > b:
-    print("answer a > b")
-elif a == b:
-    print("answer Equall")
+if b > c:
+    print(str(a),"answer a > b")
+elif b == c:
+    print(str(a),"answer Equall")
 else:
-    print("answer b > a")
+    print("answer c > b")
