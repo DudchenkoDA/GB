5cd3b7b27afd03767395688835633a2009631183
DudchenkoDA <DDA6@mail.ru>
2021-01-19 13:33:59 +0300

    Completed task 1

--- Task-1.py
+++ Task-1.py
@@ -1,1 +1,20 @@
-print("Hello")
+# Dmitrii Dudchenko
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и
+# сохраните в переменные, выведите на экран.
+print("Сalculations of integers a and b")
+a = int(input("Input int number a\n"))
+b = int(input("Input int number b\n"))
+s = a/b
+t = a % b
+
+print("answer with /", str(s))
+print("answer with % -", str(t))
+
+
+if a > b:
+    print("answer a > b")
+elif a == b:
+    print("answer Equall")
+else:
+    print("answer b > a")
