4779c4c40a191140f8f9aae102dc501a5ed46249
DudchenkoDA <DDA6@mail.ru>
2021-02-01 00:39:02 +0300

    Decised 1, 2 task

--- Lesson3/Task-1.py
+++ Lesson3/Task-1.py
@@ -1,3 +1,18 @@
+# 1. Реализовать функцию, принимающую два числа (позиционные аргументы)
+# и выполняющую их деление. Числа запрашивать у пользователя,
+# предусмотреть обработку ситуации деления на ноль.
+
+def div(*args):
+    try:
+        arg1 = int(input("Enter int number arg1"))
+        arg2 = int(input("Enter int number arg2"))
+        res = arg1 / arg2
+    except ZeroDivisionError:
+        return "Error: Zerro no division number"
+    except ValueError:
+        return "Error: You need enter int number"
+
+    return f"arg1 / arg2 = {res}"
 
 
-
+print(div())
